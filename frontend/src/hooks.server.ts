95030a43c42f2f745c652187e8f4797a0fc621f8
Chris Heriot <cheriot@gmail.com>
2022-09-18 17:24:17 -0700

    Update stuff. Do I want package-lock.json???

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -2,6 +2,6 @@ import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
   // SPA mode (https://github.com/sveltejs/kit/tree/master/packages/adapter-static#spa-mode)
-  const response = await resolve(event, { ssr: false });
+  const response = await resolve(event, {});
   return response;
 };
